6e4ef087e2c2329ba5fe1102dc02e39691ba88e8
lhalcomb <lhalcomb@drury.edu>
2025-02-10 16:23:47 -0600

    Added some comments and slightly cleaned the code. I assume from here we can train off of these data points, now that we know what they look like.

--- dataset.py
+++ dataset.py
@@ -16,10 +16,11 @@ options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.IMAGE,
 )
-
+handlandmarkoptions = HandLandmarker.create_from_options(options)
 key_points = []
 
-with HandLandmarker.create_from_options(options) as landmarker:
+
+with handlandmarkoptions as landmarker:
     img = Image.open('asl_set/1/Input Images - Sign 1/Sign 1 (1).jpeg')
     img = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(img))
     landmarks = landmarker.detect(img)
@@ -34,14 +35,17 @@ key_points = np.array(key_points)
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-for indices in [
-    [0, 1, 2, 3, 4],
-    [0, 5, 6, 7, 8],
-    [5, 9, 10, 11, 12],
-    [9, 13, 14, 15, 16],
-    [13, 17, 18, 19, 20],
-    [0, 17],
-]:
+
+
+handkeypoints = [[0, 1, 2, 3, 4], # thumb
+                 [0, 5, 6, 7, 8], #index
+                 [5, 9, 10, 11, 12], # middle
+                 [9, 13, 14, 15, 16], # ring
+                 [13, 17, 18, 19, 20], # pinky
+                 [0, 17], #wrist to pinky? 
+]
+#plotting the hand keypoints and drawing them with lines
+for indices in handkeypoints:
     xs = [key_points[i, 0] for i in indices]
     ys = [key_points[i, 1] for i in indices]
     zs = [key_points[i, 2] for i in indices]
