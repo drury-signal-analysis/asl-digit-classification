ea8f5fdb36fa9d84243c7bea9ca57a9d1ea56107
Brayden O'Neal <braydenoneal@protonmail.com>
2025-02-11 09:32:16 -0600

    Change line indices

--- dataset.py
+++ dataset.py
@@ -12,15 +12,14 @@ HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-options = HandLandmarkerOptions(
+options = HandLandmarker.create_from_options(HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.IMAGE,
-)
-handlandmarkoptions = HandLandmarker.create_from_options(options)
+))
+
 key_points = []
 
-
-with handlandmarkoptions as landmarker:
+with options as landmarker:
     img = Image.open('asl_set/1/Input Images - Sign 1/Sign 1 (1).jpeg')
     img = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(img))
     landmarks = landmarker.detect(img)
@@ -35,20 +34,21 @@ key_points = np.array(key_points)
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-
-
-handkeypoints = [[0, 1, 2, 3, 4], # thumb
-                 [0, 5, 6, 7, 8], #index
-                 [5, 9, 10, 11, 12], # middle
-                 [9, 13, 14, 15, 16], # ring
-                 [13, 17, 18, 19, 20], # pinky
-                 [0, 17], #wrist to pinky? 
+hand_landmark_indices = [
+    [0, 5, 9, 13, 17, 0],  # Palm
+    [0, 1, 2, 3, 4],  # Thumb
+    [5, 6, 7, 8],  # Index
+    [9, 10, 11, 12],  # Middle
+    [13, 14, 15, 16],  # Ring
+    [17, 18, 19, 20],  # Pinky
 ]
-#plotting the hand keypoints and drawing them with lines
-for indices in handkeypoints:
+
+# Plotting the hand landmarks and drawing them with lines
+for indices in hand_landmark_indices:
     xs = [key_points[i, 0] for i in indices]
     ys = [key_points[i, 1] for i in indices]
     zs = [key_points[i, 2] for i in indices]
+
     plt.plot(xs, ys, zs)
 
 ax.set_xlabel('X')
